882dcbd7f248d90e23042d2b185d9fafd64c4ab5
MuriloMagossi <murilomagossi@outlook.com>
2025-09-30 16:52:01 -0300

    workflow terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     # Configuração do Bucket S3
     bucket         = "terraform-state-eks-magossi"  # O nome do bucket que você criou
     key            = "global/terraform.tfstate"       # O caminho/nome do arquivo de estado dentro do bucket
-    region         = "sa-east-1"                      # A região do bucket e da tabela
+    region         = "us-east-1"                      # A região do bucket e da tabela
 
     # Configuração da Tabela DynamoDB para Lock
     dynamodb_table = "terraform-state-lock-table"     # O nome da tabela que você criou
