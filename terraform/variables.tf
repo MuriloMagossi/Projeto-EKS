5d8cc3f57b787fdf8c158d8f197b3e124dc0d44f
MuriloMagossi <murilomagossi@outlook.com>
2025-09-30 16:57:48 -0300

    corrigindo workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@
 variable "region" {
   description = "Região AWS onde os recursos serão criados"
   type        = string
-  default     = "sa-east-1"
+  default     = "us-east-1"
 }
 
 # ===================================================================
